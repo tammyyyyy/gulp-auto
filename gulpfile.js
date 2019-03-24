bd1e3cb82a60a4e9c1cfd4f803c1bf73cfe6c539
tammyyyyy <48872907+tammyyyyy@users.noreply.github.com>
2019-03-24 16:18:26 +0800

    紀錄檔

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,41 @@
-//  套件定義
-//  在package.json內引用的套件
-//  npm install gulp --global
+const gulp= require('gulp');
+const connect=require('gulp-connect');
+const gulpSass=require('gulp-sass');
+const buildSass=function(cd){
+console.log('buildSass');
+gulp.src('./src/styles/index.sass')
+.pipe(gulpSass())
+.pipe(gulp.dest('build/'));
+cb();
 
-//  gulp / yarn run gulp
-
-
-const gulp = require('gulp');
-const gulpSass = require('gulp-sass');
-
-//  ============================================================
-//          工作 1 建構SASS Compiler
-//  ============================================================
-
-
-const buildSass = function(cb){
-    console.log('buildSass');
-    gulp.src('./src/styles/index.scss')
-        .pipe(gulpSass())
-        .pipe(gulp.dest('build/'));
-    cb();
 }
-
-
-/*
- events: 'add', 'addDir', 'change', 'unlink', 'unlinkDir', 'ready', 'error', 'all
- */
-
-
-// gulp.watch('src/**/*.scss', { events: 'all' }, function(cb){
-//     console.log('change SASS');
-//     buildSass(cb);
-//     cb();
-// });
-
-
-exports.default = buildSass;
+const webServer=async function(){
+console.log('reload');
+connect.server({livereload:true});
+}
+const compressImage=async function(cb){
+console.log('compressImage');
+gulp.src('src/images/*')
+.pipe(imagemin())
+.pipe(gulp.dest('build/images'));
+}
+const webFont=async function(cb){
+console.log('webFont');
+gulp.src('./src/fonts/*')
+.pipe(gulp.dest('build/fonts/'));
+cb();
+}
+const CSSSprite=async function(cb){
+console.log('CSSSprite');
+gulp.src('src/sprite/*.png').pipe(spritesmith(params{
+imgName:'sprite.png',
+cssName:'sprite.css'}))
+.pipe(gulp.dest('build'));
+cb();
+}
+gulp.watch('src/**/*.scss',{events:'all'},function(cb){
+console.log('change SASS');
+buildSass(cb);
+cb();
+});
+exports.default=gulp.series(buildSass,webServer);
